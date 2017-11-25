8da299f4e317b0ab0780c70dc60e1621b28bfa59
ILDAR Mann <ildar.mannanov@gmail.com>
2017-11-25 13:31:37 +0300

    Level 1 / HomeWork 3. Done.

--- Level1/3l_МаннановИльдар.playground/Contents.swift
+++ Level1/3l_МаннановИльдар.playground/Contents.swift
@@ -1,19 +1,32 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
-/* Домашнее задание
-    3. Описать перечисление с возможными действиями с автомобилем - запустить/заглушить
-    двигатель, открыть/закрыть окна, погрузить/выгрузить из кузова/багажника груз определенного
-    объема.
-    4. Добавить в ваши структуры метод с одним аргументом типа вашего перечисления, который
-    будет менять свойства структуры в зависимости от действия.
-    5. Инициализировать несколько экземпляров ваших структур. Применить к ним различные
-    действия.
-    6. Вывести значения свойств экземпляров в консоль.
-*/
+
+//-------  3. Описать перечисление с возможными действиями с автомобилем - запустить/заглушить двигатель, открыть/закрыть окна, погрузить/выгрузить из кузова/багажника груз определенного объема.
+enum EngeneAction {
+    case start, stop
+}
+
+enum WindowsAction {
+    case open, close
+}
+
+/**
+ Действия с грузом.
+ */
+enum СargoAction {
+    case load(volume: Int)
+    case unload(volume: Int)
+}
+
+enum CustomError: Error {
+    case somethingWentWrong(message: String)
+}
 
 //-------  1. Описать несколько структур - любой легковой автомобиль и любой грузовик.
 //-------  2. Структуры должны содержать марку авто, год выпуска, объем багажника/кузова, стоятние запущен ли двигатель, открыты ли окна, заполненный объем багажника.
+//-------  4. Добавить в ваши структуры метод с одним аргументом типа вашего перечисления, который будет менять свойства структуры в зависимости от действия.
+
 struct Ferarri {
     // Марка
     let model: String
@@ -22,31 +35,89 @@ struct Ferarri {
     // Год выпуска
     let yearOfManufacture: Int
     // Объем багажника
-    let trunkVolume: Int = 100
+    let trunkVolume: Int
     // Заполненный объем багажника
     var filledTrunkVolume: Int
     // Текущая скорость
     var speed: Int
     // Запущен ли двигатель
-    var isEngineStarted: Bool
-    // Открыты ли окна
-    var isWindowsOpened: Bool
-    
-    func doTrunkAction(action: TrunkAction) {
-        switch action {
-        case  .load(volume):
-            if (filledTrunkVolume + volume) <= trunkVolume {
-                filledTrunkVolume += volume
-            } else {
-                //throw error
+    var isEngineStarted: Bool = false {
+        willSet {
+            if !isEngineStarted {
+               speed = 0 // Если двигатель заглох - останавливаем машину
             }
-        case  .unload(voleume):
-            
-        default:
-            //throw error
+        }
+    }
+    // Открыты ли окна
+    var isWindowsOpened: Bool = false
+    
+    init() {
+        model = "F40"
+        color = "Красный"
+        yearOfManufacture = 1992
+        trunkVolume = 5
+        filledTrunkVolume = 0
+        speed = 0
+    }
+    
+    mutating func doEngeneAction(action: EngeneAction) throws {
+        switch action {
+        case  .start:
+            guard !isEngineStarted else {
+                throw CustomError.somethingWentWrong(message: "Ошибка! Двигатель уже запущен")
+            }
+            self.isEngineStarted = true // запустим двигатель
+        case .stop:
+            guard isEngineStarted else {
+                return // Двигатель уже заглушен, игнорируем.
+            }
+            self.isWindowsOpened = false // закроем окна
+            self.isEngineStarted = false // заглушем двигатель
         }
     }
     
+    mutating func doWindowsAction(action: WindowsAction) {
+        switch action {
+        case  .open:
+            guard !isWindowsOpened else {
+                return // Окна уже открыты, игнорируем.
+            }
+            self.isWindowsOpened = true
+        case  .close:
+            guard isWindowsOpened else {
+                return // Окна уже закрыты, игнорируем.
+            }
+            self.isWindowsOpened = false
+        }
+    }
+    
+  
+    /**
+        Загрузить/выгрузить багажник.
+        ## Важно ##
+        1. Загружаемый объем может не влезть в багажник.
+        2. Если выгружаемый объем будет больше чем сейчас лежит в багажнике - будет ошибка
+     
+     - Parameter action: Вид действия направленное на багажник
+     - Returns: **true**, если операция прошла успешно; **false**, если загружаемый объем не влезает
+    */
+    mutating func doTrunkAction(action: СargoAction) throws -> Bool {
+        switch action {
+            case  let .load(volume):
+                guard (filledTrunkVolume + volume) <= trunkVolume  else {
+                    return false // Операцию выполнить не возможно, загружаемый объем не влезет
+                }
+                self.filledTrunkVolume += volume
+            case let .unload(volume):
+                guard (filledTrunkVolume - volume) <= 0  else {
+                    throw CustomError.somethingWentWrong(message: "Ошибка! Выгружаемый объем не может быть больше, чем сейчас лежит в багажнике")
+                }
+                self.filledTrunkVolume -= volume
+        }
+        return true
+    }
+    
+
 }
 struct Tatra {
     // Марка
@@ -56,7 +127,7 @@ struct Tatra {
     // Год выпуска
     let yearOfManufacture: Int
     // Объем кузова
-    let bodyVolume: Int = 200
+    let bodyVolume: Int
     // Заполненный объем кузова
     var filledBodyVolume: Int
     // Имеется ли прицеп
@@ -64,57 +135,118 @@ struct Tatra {
     // Текущая скорость
     var speed: Int
     // Запущен ли двигатель
-    var isEngineStarted: Bool
+    var isEngineStarted: Bool = false {
+        willSet {
+            if !isEngineStarted {
+                speed = 0 // Если двигатель заглох - останавливаем машину
+            }
+        }
+    }
     // Открыты ли окна
-    var isWindowsOpened: Bool
+    var isWindowsOpened: Bool = false
     
+    init() {
+        model = "815"
+        color = "Синий"
+        yearOfManufacture = 1983
+        bodyVolume = 1000
+        filledBodyVolume = 0
+        hasTrailer = false
+        speed = 0
+    }
+    
+    init(model: String,  color: String, yearOfManufacture: Int,  bodyVolume: Int,  hasTrailer: Bool) {
+        self.model = model
+        self.color = color
+        self.yearOfManufacture = yearOfManufacture
+        self.bodyVolume = bodyVolume
+        self.filledBodyVolume = 0
+        self.hasTrailer = hasTrailer
+        self.speed = 0
+    }
 
-    func doEngeneAction(action: EngeneAction) {
+    mutating func doEngeneAction(action: EngeneAction) throws {
         switch action {
-        case  .start:
-            isEngineStarted = true
-        case  .stop:
-            isEngineStarted = false
-        default:
-            //throw error
+            case  .start:
+                guard !isEngineStarted else {
+                    throw CustomError.somethingWentWrong(message: "Ошибка! Двигатель уже запущен")
+                }
+                self.isEngineStarted = true // запустим двигатель
+            case .stop:
+                guard isEngineStarted else {
+                    return // Двигатель уже заглушен, игнорируем.
+                }
+                self.isWindowsOpened = false // закроем окна
+                self.isEngineStarted = false // заглушем двигатель
         }
     }
     
-    func doWindowAction(action: WindowAction) {
+    mutating func doWindowsAction(action: WindowsAction) {
         switch action {
-        case  .open:
-            isWindowsOpened = true
-        case  .close:
-            isWindowsOpened = false
-        default:
-            //throw error
+            case  .open:
+                guard !isWindowsOpened else {
+                   return // Окна уже открыты, игнорируем.
+                }
+                self.isWindowsOpened = true
+            case  .close:
+                guard isWindowsOpened else {
+                    return // Окна уже закрыты, игнорируем.
+                }
+                self.isWindowsOpened = false
         }
     }
+    
+    /**
+     Загрузить/выгрузить кузов.
+     ## Важно ##
+     1. Загружаемый объем может не влезть в кузов.
+     2. Если выгружаемый объем будет больше чем сейчас лежит в кузове - будет ошибка
+     
+     - Parameter action: Вид действия направленное на кузов
+     - Returns: **true**, если операция прошла успешно; **false**, если загружаемый объем не влезает
+     */
+    mutating func doBodyAction(action: СargoAction) throws -> Bool {
+        switch action {
+        case  let .load(volume):
+            guard (filledBodyVolume + volume) <= bodyVolume  else {
+                return false // Операцию выполнить не возможно, загружаемый объем не влезет
+            }
+            self.filledBodyVolume += volume
+        case let .unload(volume):
+            guard (filledBodyVolume - volume) > 0  else {
+                throw CustomError.somethingWentWrong(message: "Ошибка! Выгружаемый объем не может быть больше, чем сейчас лежит в кузове")
+            }
+            self.filledBodyVolume -= volume
+        }
+        return true
+    }
+
 }
 
-
-//-------  3. Описать перечисление с возможными действиями с автомобилем - запустить/заглушить двигатель, открыть/закрыть окна, погрузить/выгрузить из кузова/багажника груз определенного объема.
-enum EngeneAction {
-    case start
-    case stop
-}
-
-enum WindowAction {
-    case open
-    case close
-}
-
-enum TrunkAction {
-    case load(volume: Int)
-    case unload(volume: Int)
-}
-print("Exercise 3:  \n")
-
-//-------  4. Добавить в ваши структуры метод с одним аргументом типа вашего перечисления, который будет менять свойства структуры в зависимости от действия.
-print("Exercise 4:  \n")
 
 //-------  5. Инициализировать несколько экземпляров ваших структур. Применить к ним различные действия.
-print("Exercise 5:  \n")
-
 //-------  6. Вывести значения свойств экземпляров в консоль.
-print("Exercise 6:  \n")
+var myFerrari: Ferarri = Ferarri()
+try myFerrari.doEngeneAction(action: .start)
+myFerrari.doWindowsAction(action: .open)
+myFerrari.speed = 200
+print("MyFerrari, Заведен, открыты окна и едет: \(myFerrari) \n")
+let loadResult = try myFerrari.doTrunkAction(action: .load(volume: 10))
+print("Попытка загрузки в Феррари, превышающего предел, багажа объемом 10: \(loadResult) \n")
+try myFerrari.doEngeneAction(action: .stop)
+print("MyFerrari, Заглушен: \(myFerrari) \n")
+
+var myTatra: Tatra = Tatra(model: "148", color: "Blue", yearOfManufacture: 1972, bodyVolume: 950, hasTrailer: false)
+try myTatra.doEngeneAction(action: .start)
+try myTatra.doBodyAction(action: .load(volume: 500))
+myTatra.speed = 200
+print("myTatra, Заведена, загружена и едет: \(myTatra) \n")
+myTatra.isEngineStarted = false
+try myTatra.doBodyAction(action: .unload(volume: 400))
+print("myTatra, заглушена и разгружена не до конца: \(myTatra) \n")
+
+// генерируем не позволительную выгрузку груза
+try! myTatra.doBodyAction(action: .unload(volume: 200))
+
+
+
