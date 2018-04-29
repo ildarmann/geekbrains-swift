74e80816c91224edca0c110cd2a27748398f6813
Маннанов Ильдар Фаварисович <IFMannanov@sberbank.ru>
2018-04-30 00:46:08 +0400

    Level 2 / HomeWork 3/ WeatherApp/ рефакторинг - рефакторинг структуры модулей в проекте - попрактиковался с таблицами

---
+++ Level2/temp/temp/view/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  temp
+//
+//  Created by Ильдар on 26.04.2018.
+//  Copyright © 2018 mif. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var cellName: UILabel!
+}
