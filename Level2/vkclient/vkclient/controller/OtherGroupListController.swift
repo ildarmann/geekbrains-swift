93bcda20be62cd421dea96bd9154717ff5bf03e1
ILDAR Mann <ildar.mannanov@gmail.com>
2018-06-21 23:54:12 +0300

    Level 2 / HomeWork 3/ VKClientApp. - на странице Чужих групп добавлен поиск по списку чужих групп

--- Level2/vkclient/vkclient/controller/OtherGroupListController.swift
+++ Level2/vkclient/vkclient/controller/OtherGroupListController.swift
@@ -15,10 +15,24 @@ private let OTHER_GROUP_LIST_CELL_ID = "OtherGroupListCellID"
 class OtherGroupListController: UIViewController {
     //let vkDataProvider: VkDataProvider = VkDataProvider()
     
+    @IBOutlet weak var tableView: UITableView!
+    
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    var otherGroupsToShow: [VkGroup]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Setup the Search Controller
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Введите название группы"
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
 
         // Do any additional setup after loading the view.
+        otherGroupsToShow = VkDataProvider.getOtherGroups()
     }
 
     override func didReceiveMemoryWarning() {
@@ -40,6 +54,7 @@ class OtherGroupListController: UIViewController {
 }
 
 extension OtherGroupListController: UITableViewDataSource {
+    
     func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
@@ -47,8 +62,11 @@ extension OtherGroupListController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return VkDataProvider.getOtherGroups().count
-
+        if let otherGroups = otherGroupsToShow {
+            return otherGroups.count
+        } else {
+            return 0
+        }
     }
     
     
@@ -56,12 +74,27 @@ extension OtherGroupListController: UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: OTHER_GROUP_LIST_CELL_ID, for: indexPath) as! OtherGroupListCell
         
         
-        let otherGroup = VkDataProvider.getOtherGroups()[indexPath.row]
-        cell.name.text = otherGroup.name
-        cell.foto.image = otherGroup.foto
-        cell.memberCount.text = String(otherGroup.memberCount)
+        if let otherGroups = otherGroupsToShow {
+            let otherGroup = otherGroups[indexPath.row]
+            cell.name.text = otherGroup.name
+            cell.foto.image = otherGroup.foto
+            cell.memberCount.text = String(otherGroup.memberCount)
+        }
         return cell
 
+    }
+    
+    
+    func filterGroupsByName(_ searchText: String)  {
+        print("filterGroupsByName.searchText = \(searchText)")
+        if searchText.isEmpty {
+            otherGroupsToShow = VkDataProvider.getOtherGroups()
+        } else {
+            otherGroupsToShow = VkDataProvider.getOtherGroups().filter({( group : VkGroup) -> Bool in
+                return group.name.lowercased().contains(searchText.lowercased())
+            })
+        }
+        tableView.reloadData()
     }
         
 }
@@ -71,8 +104,17 @@ extension OtherGroupListController: UITableViewDelegate {
     // срабатывает при выборе строки в таблице
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("нажата строка № \(indexPath.row) в секции \(indexPath.section)")
-        let selectedGroup = VkDataProvider.getOtherGroups()[indexPath.row]
-        VkDataProvider.addToMyGroups(selectedGroup.id) // добавили выбранную группу в список моих групп
+        if let otherGroups = otherGroupsToShow {
+            let selectedGroup = otherGroups[indexPath.row]
+            VkDataProvider.addToMyGroups(selectedGroup.id) // добавили выбранную группу в список моих групп
+        }
+       
+    }
+}
+
+extension OtherGroupListController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        filterGroupsByName(searchController.searchBar.text!)
     }
 }
 
